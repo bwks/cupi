758bddc7fc391bccb6163e3271b2ed0943929a2f
brad <sebradle@cisco.com>
2015-12-28 20:54:07 +1100

    added some get methods and minor refactoring

--- cake.py
+++ cake.py
@@ -11,17 +11,17 @@ class CUPI(object):
     >>> c = CUPI('192.168.200.11', 'username', 'password')
     >>> c.get_owner_location_oid()
     >>> '89443b75-0547-4008-8245-39c3abeaed31'
-    
     """
     def __init__(self, cuc, username, password):
         """
-        Init sets up the connection to Cisco Unity Connection
+        Sets up the connection to Cisco Unity Connection
 
         :param cuc: Unity connection IP Address
         :param username: User with privilege to access rest api
         :param password: Users password
         """
-        self.url_base = 'https://{0}/vmrest/'.format(cuc)
+
+        self.url_base = 'https://{0}/vmrest'.format(cuc)
         self.cuc = requests.session()
         self.cuc.auth = (username, password)
         self.cuc.verify = False
@@ -32,7 +32,8 @@ class CUPI(object):
         Get the owner location oid. This is needed for creating schedules
         :return: owner location oid
         """
-        url = '{0}locations/connectionlocations'.format(self.url_base)
+
+        url = '{0}/locations/connectionlocations'.format(self.url_base)
         return self.cuc.get(url).json()['ConnectionLocation']['ObjectId']
 
     def get_call_handler_template_oid(self):
@@ -40,8 +41,30 @@ class CUPI(object):
         Method to get the call handler template oid
         :return: call handler template oid
         """
-        url = '{0}callhandlertemplates'.format(self.url_base)
+
+        url = '{0}/callhandlertemplates'.format(self.url_base)
         return self.cuc.get(url).json()['CallhandlerTemplate']['ObjectId']
+
+    def get_user_call_handler_oid(self, user_oid):
+        """
+        Get the oid of a call handler assigned to a user
+        :param user_oid: oid of the user to get the call handler oid
+        :return: users call handler oid
+        """
+
+        url = '{0}/users/{1}'.format(self.url_base, user_oid)
+        return self.cuc.get(url).json()['CallHandlerObjectId']
+
+    def get_users(self):
+        """
+        Get all users
+        :return: A list of user dictionaries
+        """
+
+        url = '{0}/users'.format(self.url_base)
+        return self.cuc.get(url).json()['User']
+
+
 
 
 
