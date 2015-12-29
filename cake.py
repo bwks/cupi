0a09b2c06d94ebfc5ccd69f767bba9d7e94184da
brad <sebradle@cisco.com>
2015-12-29 11:18:53 +1100

    added method to add a schedule and delete schedule and schedule sets

--- cake.py
+++ cake.py
@@ -1,7 +1,7 @@
 """
 Class to interface with cisco unity connection cupi api.
 Author: Brad Searle
-Version: 0.1
+Version: 0.2
 """
 
 import json
@@ -121,23 +121,144 @@ class CUPI(object):
         else:
             return resp
 
-    def add_schedule(self, display_name, owner_location_oid):
+    def add_schedule(self,
+                     display_name,
+                     owner_location_oid,
+                     is_holiday='false',
+                     start_time='510',
+                     end_time='1020',
+                     is_active_monday='true',
+                     is_active_tuesday='true',
+                     is_active_wednesday='true',
+                     is_active_thursday='true',
+                     is_active_friday='true',
+                     is_active_saturday='false',
+                     is_active_sunday='false'):
+        """
+        To add a schedule there are 4 steps
+        1) Add a schedule set
+        2) Add a schedule
+        3) Map the schedule to the scheduleset with a schedule set map
+        4) Add a schedule detail to the schedule
+        Default schedule times are Monday - Friday 8.30am - 5.00pm
+        :param display_name: Schedule display name
+        :param owner_location_oid: Get this oid with the get_owner_location_oid method
+        :param is_holiday: Is this schedule a holiday schedule true/false
+        :param start_time: Schedule start time
+        :param end_time: Schedule end time
+        :param is_active_monday: Schedule active Monday true/false
+        :param is_active_tuesday: Schedule active Tuesday true/false
+        :param is_active_wednesday: Schedule active Wednesday true/false
+        :param is_active_thursday: Schedule active Thursday true/false
+        :param is_active_friday: Schedule active Friday true/false
+        :param is_active_saturday: Schedule active Saturday true/false
+        :param is_active_sunday: Schedule active Sunday true/false
+        :return: Result of adding the schedule
+
+        example usage:
+                                    display_name                         owner_location_oid
+        >>> c.add_schedule('test_schedule M-F 8.30am - 5.00pm', '89443b75-0547-4008-8245-39c3abeaed31')
+        >>> 'Schedule Successfully Added'
         """
 
-        :param display_name:
-        :param owner_location_oid:
-        :return:
-        """
-
+        # 1) Add a schedule set
+        url = '{0}/schedulesets'.format(self.url_base)
         body = {
             'DisplayName': display_name,
             'OwnerLocationObjectId': owner_location_oid,
         }
-        url = '{0}/schedulesets'.format(self.url_base)
 
-        resp = self.cuc.post(url, body=body)
+        resp = self.cuc.post(url, json=body)
         schedule_set_oid = resp.text.split('/')[-1]
+        print(schedule_set_oid)
 
-        return schedule_set_oid
+        if resp.status_code != 201:
+            return 'Could not add schedule set {0} {1}'.format(resp.status_code, resp.reason)
 
+        # 2) Add a schedule
+        else:
+            url = '{0}/schedules'.format(self.url_base)
+            body = {
+                'DisplayName': display_name,
+                'OwnerLocationObjectId': owner_location_oid,
+                'IsHoliday': is_holiday
+            }
 
+            resp = self.cuc.post(url, json=body)
+            schedule_oid = resp.text.split('/')[-1]
+            print(schedule_oid)
+
+            if resp.status_code != 201:
+                return 'Could not add schedule {0} {1}'.format(resp.status_code, resp.reason)
+
+            # 3) Map the schedule to the schedule set with a schedule set map
+            else:
+                url = '{0}/schedulesets/{1}/schedulesetmembers'.format(self.url_base, schedule_set_oid)
+                body = {
+                    'ScheduleSetObjectId': schedule_set_oid,
+                    'ScheduleObjectId': schedule_oid,
+                    'Exclude': 'false'
+                }
+
+                resp = self.cuc.post(url, json=body)
+
+                if resp.status_code != 201:
+                    return 'Could not map schedule to schedule set {0} {1}'.format(resp.status_code, resp.reason)
+
+                # 4) Add a schedule detail to the schedule
+                else:
+                    url = '{0}/schedules/{1}/scheduledetails'.format(self.url_base, schedule_oid)
+                    body = {
+                        'Subject': display_name,
+                        'StartTime': start_time,
+                        'EndTime': end_time,
+                        'IsActiveMonday': is_active_monday,
+                        'IsActiveTuesday': is_active_tuesday,
+                        'IsActiveWednesday': is_active_wednesday,
+                        'IsActiveThursday': is_active_thursday,
+                        'IsActiveFriday': is_active_friday,
+                        'IsActiveSaturday': is_active_saturday,
+                        'IsActiveSunday': is_active_sunday,
+                    }
+
+                    resp = self.cuc.post(url, json=body)
+
+                    if resp.status_code != 201:
+                        return 'Could not add schedule detail {0} {1}'.format(resp.status_code, resp.reason)
+
+                    else:
+                        return 'Schedule Successfully Added'
+
+    def delete_schedule_set(self, schedule_set_oid):
+        """
+
+        :param schedule_set_oid:
+        :return:
+        """
+
+        url = '{0}/schedulesets/{1}'.format(self.url_base, schedule_set_oid)
+
+        resp = self.cuc.delete(url)
+        if resp.status_code == 204:
+            return 'Schedule set deleted'
+        elif resp.status_code == 404:
+            return 'Schedule set not found'
+        else:
+            return 'Unknown Result {0} {1}'.format(resp.status_code, resp.reason)
+
+    def delete_schedule(self, schedule_oid):
+        """
+
+        :param schedule_oid:
+        :return:
+        """
+
+        url = '{0}/schedules/{1}'.format(self.url_base, schedule_oid)
+
+        resp = self.cuc.delete(url)
+        if resp.status_code == 204:
+            return 'Schedule deleted'
+        elif resp.status_code == 404:
+            return 'Schedule not found'
+        else:
+            return 'Unknown Result {0} {1}'.format(resp.status_code, resp.reason)
