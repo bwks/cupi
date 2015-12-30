67b8e8960f1118ceea2e504fc98777074e5fc455
brad <sebradle@cisco.com>
2015-12-30 22:27:21 +1100

    updated tests

--- tests/test_cupi.py
+++ tests/test_cupi.py
@@ -3,10 +3,12 @@ Unity Connection server will be required for these tests
 Tests are performed against CUC 10.5.2
 """
 import unittest
+import requests
 
 from requests.exceptions import ConnectTimeout
-
 from cupi.cake import CUPI
+
+requests.packages.urllib3.disable_warnings()
 
 
 class TestCUPI(unittest.TestCase):
@@ -16,10 +18,32 @@ class TestCUPI(unittest.TestCase):
 
     def test_connection_to_cuc_server_timeout_with_incorrect_ip_fail(self):
         c = CUPI('192.168.200.111', 'admin', 'asdfpoiu')
-        self.assertRaises(ConnectTimeout, c.get_owner_location_oid())
+        self.assertRaises(ConnectTimeout, c.online_test)
 
-    def test_connection_to_cuc_server_timeout_with_correct_ip_pass(self):
+    def test_connection_to_cuc_server_timeout_with_correct_ip(self):
         self.assertEqual(self.cuc.online_test(), 200)
 
+    def test_get_languages_method_returns_200_ok_and_dict_key_exists(self):
+        result = self.cuc.get_languages()
+        self.assertEqual(result[0], 200) and 'LanguageMapping' in result[1]
 
+    def test_get_owner_location_oid_returns_200_ok_and_oid_string(self):
+        result = self.cuc.get_owner_location_oid()
+        self.assertEqual(result[0], 200) and result[1] != ''
+
+    def test_get_schedule_sets_method_full_returns_200_ok_and_dict_key_exists(self):
+        result = self.cuc.get_schedule_sets(mini=False)
+        self.assertEqual(result[0], 200) and 'ScheduleSet' in result[1]
+
+    def test_get_schedule_sets_method_mini_returns_list(self):
+        result = self.cuc.get_schedule_sets(mini=True)
+        self.assertTrue(isinstance(result, list))
+
+    def test_get_schedules_method_full_returns_200_ok_and_dict_key_exists(self):
+        result = self.cuc.get_schedules(mini=False)
+        self.assertEqual(result[0], 200) and 'Schedule' in result[1]
+
+    def test_get_schedules_method_mini_returns_list(self):
+        result = self.cuc.get_schedules(mini=True)
+        self.assertTrue(isinstance(result, list))
 
